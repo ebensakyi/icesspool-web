b237e05eb69e2ff643c12dfee8b3affa2cd73337
Kofi Agyemang <ebensakyi@gmail.com>
2023-11-17 22:21:25 +0000

    -

--- prisma/seeds/service_area.ts
+++ prisma/seeds/service_area.ts
@@ -1,11 +1,9 @@
 export const serviceAreas = [
-    {
-      name: "Greater Accra Area",
-      cityPolygon:"[67.25075122775252,35.9854261285504],[67.25075122775252,35.9854261285504]",
-      regionId:1,
-      serviceId:1
-    },  
-  
-   
-  ];
-  
+  {
+    name: "Greater Accra Area",
+    cityPolygon:
+      "[5.472543, -0.420407],[5.797806, -0.549497],[6.116055, 0.216800],[5.781411, 0.694705]",
+    regionId: 1,
+    serviceId: 1,
+  },
+];

0ab3ac06a24705f56da6cda3377d447c9ddb25a4
Kofi Agyemang <ebensakyi@gmail.com>
2023-12-02 23:53:01 +0000

    -

---
+++ src/app/user/operator/page.tsx
@@ -0,0 +1,25 @@
+export const dynamic = "force-dynamic";
+
+import { getAdmins, getPages, getServiceAreas, getUserTypes } from "@/src/api-services";
+import Admin from "@/src/components/user/Admin";
+import { headers } from "next/headers";
+
+
+
+
+export default async function Page({ searchParams }: any) {
+
+
+    const pages = await getPages()
+    const userTypes = await getUserTypes()
+    const serviceAreas = await getServiceAreas(searchParams)
+    const users = await getAdmins()
+
+    let data = { pages, userTypes,serviceAreas,users }
+
+
+
+    return <Admin data={data} />
+
+
+}

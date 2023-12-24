4bd22d40247fb4cca9bf0786e05c5818f421f927
Kofi Agyemang <ebensakyi@gmail.com>
2023-12-24 00:46:16 +0000

    -

--- src/app/api/services/biodigester/route.ts
+++ src/app/api/services/biodigester/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import { prisma } from "@/prisma/db";
 import { logActivity } from "@/libs/log";
 import { getServerSession } from "next-auth";
-import { authOptions } from "../auth/[...nextauth]/options";
+import { authOptions } from "../../auth/[...nextauth]/options";
 
 export async function POST(request: Request) {
   try {
@@ -65,7 +65,7 @@ export async function GET(request: Request) {
 
     // await logActivity("Visited data assignment page", session?.user?.id);
 
-    const response = await prisma.service.findMany({
+    const response = await prisma.biodigesterService.findMany({
       where: { deleted: 0 },
     });
 

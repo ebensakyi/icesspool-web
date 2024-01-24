012c757f7c62d8519d743cfbf250ccaf01c76b88
Kofi Agyemang <ebensakyi@gmail.com>
2024-01-24 23:33:56 +0000

    -

--- next.config.js
+++ next.config.js
@@ -3,9 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
    swcMinify:false,
 
-  rules: {
-    "@next/next/no-sync-scripts": "off",
-  },
+  // rules: {
+  //   "@next/next/no-sync-scripts": "off",
+  // },
   images: {
     domains: ['icesspool-files.s3.amazonaws.com','esicapps-exports.s3.eu-west-2.amazonaws.com',],
   },

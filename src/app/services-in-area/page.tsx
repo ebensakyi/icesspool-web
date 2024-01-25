7c8f5dc781163aba613f470f5375897ee58c5441
Kofi Agyemang <ebensakyi@gmail.com>
2024-01-25 10:17:22 +0000

    -

--- src/app/services-in-area/page.tsx
+++ src/app/services-in-area/page.tsx
@@ -1,10 +1,59 @@
 export const dynamic = "force-dynamic";
 
+import { SERVER_BASE_URL } from '@/config';
 import { ServicesInArea } from '@/src/components/ServicesInArea';
+import { headers } from 'next/headers';
 
 
 
 
+async function getServices(searchParams: any) {
+    let { searchText } = searchParams;
+  
+    let { page } = searchParams;
+  
+    const res = await fetch(
+      `${SERVER_BASE_URL}/api/services?page=${page}&searchText=${searchText}`,
+      { cache: "no-store", headers: headers() }
+    );
+  
+    if (!res.ok) {
+      throw new Error("Failed to fetch data");
+    }
+  
+    return res.json();
+  }
+
+  async function getServicesInAreas(searchParams: any) {
+    let response = await fetch(
+      `${SERVER_BASE_URL}/api/services-in-area`,
+      { cache: "no-store" }
+    );
+  
+    if (!response.ok) {
+      throw new Error("Failed to fetch data");
+    }
+    return await response.json();
+  }
+  
+
+ async function getServiceAreas(searchParams: any) {
+    try {
+       let response = await fetch(`${SERVER_BASE_URL}/api/service-area`, {
+        cache: "no-store",
+      });
+    
+      if (!response.ok) {
+        throw new Error("Failed to fetch data");
+      }
+      return await response.json();
+    } catch (error) {
+      console.log("getServiceAreas==>");
+      
+    }
+    
+     
+    }
 
 export default async function Page({ searchParams }: any) {
     const services = await getServices(searchParams)
--- src/app/user/admin/page.tsx
+++ src/app/user/admin/page.tsx
@@ -1,10 +1,61 @@
 export const dynamic = "force-dynamic";
 
-import { getAdmins, getPages, getServiceAreas, getUserTypes } from "@/src/app/api-services";
+import { SERVER_BASE_URL } from "@/config";
 import Admin from "@/src/components/user/Admin";
 import { headers } from "next/headers";
 
+async function getServiceAreas(searchParams: any) {
+    try {
+        let response = await fetch(`${SERVER_BASE_URL}/api/service-area`, {
+            cache: "no-store",
+        });
 
+        if (!response.ok) {
+            throw new Error("Failed to fetch data");
+        }
+        return await response.json();
+    } catch (error) {
+        console.log("getServiceAreas==>");
+
+    }
+
+
+}
+
+
+async function getUserTypes() {
+
+    let response = await fetch(`${SERVER_BASE_URL}/api/primary-data/user-type`, { cache: 'no-store', headers: headers() });
+    if (!response.ok) {
+        throw new Error('Failed to fetch data')
+    }
+    return await response.json();
+
+}
+
+
+async function getPages() {
+
+    let response = await fetch(`${SERVER_BASE_URL}/api/primary-data/pages`, { cache: 'no-store', headers: headers() });
+
+    if (!response.ok) {
+        throw new Error('Failed to fetch data')
+    }
+    return await response.json();
+
+}
+
+
+async function getAdmins() {
+
+    let response = await fetch(`${SERVER_BASE_URL}/api/user/admin`, { cache: 'no-store', headers: headers() });
+
+    if (!response.ok) {
+        throw new Error('Failed to fetch data')
+    }
+    return await response.json();
+
+}
 
 
 export default async function Page({ searchParams }: any) {
@@ -15,7 +66,7 @@ export default async function Page({ searchParams }: any) {
     const serviceAreas = await getServiceAreas(searchParams)
     const users = await getAdmins()
 
-    let data = { pages, userTypes,serviceAreas,users }
+    let data = { pages, userTypes, serviceAreas, users }
 
 
 

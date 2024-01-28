844036d5737f3f38512e03a5bf6e82b59549ad1f
Kofi Agyemang <ebensakyi@gmail.com>
2024-01-28 19:18:04 +0000

    -

--- config/index.ts
+++ config/index.ts
@@ -3,7 +3,7 @@
 
 const dev = process.env.NODE_ENV !== 'production';
 
-export const SERVER_BASE_URL = dev ? 'http://localhost:3000' : 'https://new.icesspool.net'
+export const SERVER_BASE_URL = dev ? 'http://127.0.0.1:3000' : 'https://new.icesspool.net'
 export const LOGIN_URL = dev ? "/auth/login" : "https://new.icesspool.net/auth/login"
 
 export const AWS_S3_URL = 'https://icesspool-files.s3.amazonaws.com/uploads/'

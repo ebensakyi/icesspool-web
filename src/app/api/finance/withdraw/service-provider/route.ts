8c50675220d4c9694cd6cb57202f44bd0a5737ff
Kofi Agyemang <ebensakyi@gmail.com>
2024-04-27 17:14:38 +0000

    -

--- src/app/api/finance/withdraw/service-provider/route.ts
+++ src/app/api/finance/withdraw/service-provider/route.ts
@@ -10,17 +10,11 @@ export async function POST(request: Request) {
     let userId = Number(res.userId);
     let amount = Number(res.amount);
 
-console.log(userId);
-
-
     const sp = await prisma.serviceProvider.findFirst({
       where: { deleted: 0, userId: userId },
     });
 
     let serviceProviderId: any = sp?.id;
-
-    console.log("serviceProviderId==>serviceProviderId", serviceProviderId);
-    
 
     let x = await prisma.serviceProviderWithdrawal.create({
       data: {
@@ -58,6 +52,36 @@ export async function GET(request: Request) {
     });
 
     return NextResponse.json({ pendingWithdrawal });
+  } catch (error) {
+    console.log(error);
+
+    return NextResponse.json(error);
+  }
+}
+
+export async function PUT(request: Request) {
+  try {
+    const res = await request.json();
+
+    let id = Number(res.userId);
+
+    let sp = await prisma.serviceProvider.findFirst({ where: { userId: id } });
+    let withdrawal = await prisma.serviceProviderWithdrawal.findFirst({
+      orderBy: {
+        id:'desc'
+      },
+      where: { serviceProviderId: sp?.id, deleted: 0 },
+    });
+    console.log(withdrawal);
+
+    await prisma.serviceProviderWithdrawal.update({
+      data: { deleted: 1 },
+      where: {
+        id: withdrawal?.id,
+      },
+    });
+
+    return NextResponse.json({});
   } catch (error) {
     console.log(error);
 

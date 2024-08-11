34e214c55fd8df62e63f2b171be1f64f06230c84
Kofi Agyemang <ebensakyi@gmail.com>
2024-08-11 21:14:48 +0000

    -

---
+++ src/app/api/service-request/toilet-truck/route.ts
@@ -0,0 +1,29 @@
+export const dynamic = "force-dynamic";
+
+import { NextResponse } from "next/server";
+import { prisma } from "@/prisma/db";
+import { logActivity } from "@/libs/log";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/options";
+
+export async function GET(request: Request) {
+  try {
+    let { searchParams } = new URL(request.url);
+
+    let userId = Number(searchParams.get("userId"));
+
+    const session: any = await getServerSession(authOptions);
+
+    // await logActivity("Visited data assignment page", session?.user?.id);
+
+    const response = await prisma.transaction.findMany({
+      where: { deleted: 0, serviceProviderId: userId, currentStatus: 6 },
+    });
+
+    return NextResponse.json({ response });
+  } catch (error) {
+    console.log(error);
+
+    return NextResponse.json(error);
+  }
+}

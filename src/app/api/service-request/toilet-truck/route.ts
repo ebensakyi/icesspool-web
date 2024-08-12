2144b384e6c3380d82febb0dd6496c0bc8f348b7
Kofi Agyemang <ebensakyi@gmail.com>
2024-08-12 11:21:38 +0000

    -

--- src/app/api/service-request/toilet-truck/route.ts
+++ src/app/api/service-request/toilet-truck/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 import { prisma } from "@/prisma/db";
 import { logActivity } from "@/libs/log";
 import { getServerSession } from "next-auth";
-import { authOptions } from "../auth/[...nextauth]/options";
+import { authOptions } from "../../auth/[...nextauth]/options";
 
 export async function GET(request: Request) {
   try {
